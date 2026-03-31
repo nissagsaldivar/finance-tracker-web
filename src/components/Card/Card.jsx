27e8e54da414895cc667eb8affb5cea0059d5331
Brandon Abbott <pbabbott@gmail.com>
2026-03-31 02:42:32 +0000

    Setup empty re-usable components

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,5 @@
+export default function Card({children}) {
+    return null;
+}
+// just the pink box
+// don't put card header text in here
---
+++ src/components/CardHeaderText/CardHeaderText.jsx
@@ -0,0 +1,4 @@
+export default function CardHeaderText({children}) {
+    return null;
+}
+//  Just the label. frequently used at the top of Card components. 
